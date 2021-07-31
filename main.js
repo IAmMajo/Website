17db9004ca3f82acc805b60e0f7b6fec1374cebd
Majo <git@maxoverlack.dev>
2021-07-31 17:16:30 +0200

    Add accounts

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-import "./style.css";
+import { MDCRipple } from "@material/ripple";
+import "./style.scss";
 
-document.querySelector("#app").innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`;
+document.querySelectorAll(".mdc-card").forEach((element) => {
+  new MDCRipple(element);
+});
