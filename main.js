23d08f6304db658dd762c97787baa1255152b5cb
Majo <git@maxoverlack.dev>
2021-08-01 14:49:09 +0200

    Self-host Roboto font

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 import { MDCRipple } from "@material/ripple";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/400.css";
 import "./style.scss";
 
 document.querySelectorAll(".mdc-card").forEach((element) => {
