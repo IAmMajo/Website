138989592e6322a71d788b311a80a441fbaefd02
Majo <git@maxoverlack.dev>
2024-08-18 21:40:02 +0200

    Switch to Astro and mdui + Update everything + New theme + Small design changes + Remove 3DS accounts

--- main.js
+++
@@ -1,8 +0,0 @@
-import { MDCRipple } from "@material/ripple";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/400.css";
-import "./style.scss";
-
-document.querySelectorAll(".mdc-card").forEach((element) => {
-  new MDCRipple(element);
-});
--- vite.config.js
+++
@@ -1,4 +0,0 @@
-import { defineConfig } from "vite";
-import compress from "vite-plugin-compress";
-
-export default defineConfig({ plugins: [compress({ brotli: false })] });
